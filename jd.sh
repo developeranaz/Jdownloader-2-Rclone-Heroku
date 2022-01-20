d2989f2688b19c10ccef2cb7d7dbffcd9612ff29
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-20 20:31:18 +0530

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -6,5 +6,7 @@ curl 'https://gitlab.com/dev-extended/developeranaz-filehoster/-/raw/main/JDownl
 tar -xf /jd.tar.gz
 cd /jdx
 cat /Jdownloader-2-Rclone-Heroku/cr.json |sed "s|THEJDUSERPASSWORD|$JD_USER_PASSWORD|g" |sed "s|THEJDUSEREMAIL|$JD_USER_EMAIL|g" >"/jdx/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
+cat /Jdownloader-2-Rclone-Heroku/ex.json > "/jdx/cfg/org.jdownloader.extensions.eventscripter.EventScripterExtension.json"
+cat /Jdownloader-2-Rclone-Heroku/exs.json > "/jdx/cfg/org.jdownloader.extensions.eventscripter.EventScripterExtension.scripts.json"
 java -jar JDownloader.jar -norestart -noerr -r
 done
