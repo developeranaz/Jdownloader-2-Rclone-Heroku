b4fed324c5f22881556854027a0a6a29e65e7e2e
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-20 17:21:22 +0530

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -2,5 +2,13 @@
 while :
 do
 cd /
-java -jar /JDownloader.jar -norestart -noerr -r
+#!/bin/bash
+fileid="1APyWX-f09aWwnwThjo-eKxu_uyPb3uvL"
+filename="jd.tar.gz"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+cat /Jdownloader-2-Rclone-Heroku/cr.json |sed "s|THEJDUSERPASSWORD|$JD_USER_PASSWORD|g" |sed "s|THEJDUSEREMAIL|$JD_USER_EMAIL|g" >"/jdx/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
+tar -xf /jd.tar.gz
+cd /jdx
+java -jar JDownloader.jar -norestart -noerr -r
 done
