c84e0eb28384eb139b461b3a7619bd39785a795e
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-23 00:33:02 +0530

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 chmod +x /Jdownloader-2-Rclone-Heroku/up.sh
+cp /jdx/cfg/org.jdownloader.settings.GeneralSettings.json /jdx
+cat /jdx/org.jdownloader.settings.GeneralSettings.json |sed 's/"defaultdownloadfolder" : "\/jdx/"defaultdownloadfolder" : "\/jdx\/jdownload/g'
 while :
 do
 cd /
