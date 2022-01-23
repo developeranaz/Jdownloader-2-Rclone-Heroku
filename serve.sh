4b6a3bfe531e68f1dd756915aa84484b19b36a5a
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-23 20:49:59 +0530

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "$PORT" >/PORT
+RPORT="$(cat /PORT)"
 curl 'https://raw.githubusercontent.com/developeranaz/Jdownloader-2-Rclone-Heroku/main/scripts/rclonealias.conf' >/alias.conf
-rclone rcd --rc-serve --config=/alias.conf
+rclone rcd --rc-serve --config=/alias.conf --rc-addr=0.0.0.0:$RPORT
