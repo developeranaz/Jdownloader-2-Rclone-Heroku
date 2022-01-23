3af1994ce50d6a21746a79cf4e40bee1b68f80b8
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-23 09:53:45 +0530

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 curl "$conf_in_url" >/rclone.conf
+firstremote=$(rclone listremotes |head -1 |sed 's/://g')
 
 while getopts ":a:" opt; do
   case $opt in
     a)
-      rclone copy "/jdx/jdownload/$OPTARG" "$(rclone listremotes --config=/rclone.conf |head -1)jd2rc" --config=/rclone.conf;
-     # echo "$OPTARG" >/UOP
+      rclone copy "/jdx/jdownload/$OPTARG" "$firstremote:jd2rc" --config=/rclone.conf;
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
