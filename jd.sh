2ae0994f6299eb86bc03b468433a86de27d26e6d
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-23 09:23:37 +0530

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -13,5 +13,5 @@ cd /jdx
 cat /Jdownloader-2-Rclone-Heroku/cr.json |sed "s|THEJDUSERPASSWORD|$JD_USER_PASSWORD|g" |sed "s|THEJDUSEREMAIL|$JD_USER_EMAIL|g" >"/jdx/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
 cat /Jdownloader-2-Rclone-Heroku/ex.json > "/jdx/cfg/org.jdownloader.extensions.eventscripter.EventScripterExtension.json"
 cat /Jdownloader-2-Rclone-Heroku/exs.json > "/jdx/cfg/org.jdownloader.extensions.eventscripter.EventScripterExtension.scripts.json"
-java -jar JDownloader.jar -norestart -noerr -r
+java -jar /jdx/JDownloader.jar -norestart -noerr -r
 done
