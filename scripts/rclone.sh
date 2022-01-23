35fea8c3b8ea9a528093b6aa0100a03c255bba4f
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-23 16:08:32 +0530

    Update rclone.sh

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -4,5 +4,5 @@ while :
 do
 sleep 10
 thelog="$(ls /jdx/logs |grep '2022' |tail -1)"
-cat /jdx/logs/"$thelog"/* |grep --colour 'DownloadCandidate:' |sed 's/DownloadCandidate:/\nFILE="/g' |sed 's/@/"\n/g' |grep 'FILE="' |sort -u |sed 's|$|; rclone copy "/jdx/jdownload/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf; rclone copy "/jdx/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf|g' |bash
+cat /jdx/logs/"$thelog"/* |grep 'DownloadCandidate:' |sed 's/DownloadCandidate:/\nFILE="/g' |sed 's/@/"\n/g' |grep 'FILE="' |sort -u |sed 's|$|; rclone copy "/jdx/jdownload/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf; rclone copy "/jdx/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf|g' |bash
 done
