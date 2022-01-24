ebec07718cad360120889821f3df786cc0e0721c
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-24 23:29:00 +0530

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #VPS
 echo "$PORT" >/PORT
+
+cat /Jdownloader-2-Rclone-Heroku/scripts/nginxconf |sed "s/THERANDOMPORT/$(cat /PORT)/g" >/etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
 RPORT="$(cat /PORT)"
 curl 'https://raw.githubusercontent.com/developeranaz/Jdownloader-2-Rclone-Heroku/main/scripts/rclonealias.conf' >/alias.conf
 rclone rcd --rc-serve --config=/alias.conf --rc-addr=0.0.0.0:$RPORT
