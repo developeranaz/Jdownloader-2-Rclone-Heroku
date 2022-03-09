1c64d65856bd12a42b5b2dafed04f5a8743f4ab1
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-09 12:01:06 +0530

    Update rclone.sh

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -7,7 +7,7 @@ sleep 30
 rclone ls /jdx/downloads >/home/up_list
 grep -v -x -f /home/up_ed_list /home/up_list |sed 's/^ *//g' |sed '/\.part/d' >/home/up_ing_list
 cat /home/up_ing_list |sed 's/^/+ /g'>/home/uploading_list
-rclone copy /jdx/downloads $(cat /remote): --filter-from=/home/up_ing_list --checkers 1 --transfers 1 --use-mmap --buffer-size 0M --tpslimit 1 --no-traverse --config=/home/rclone.conf
+rclone copy /jdx/downloads $(cat /remote): --filter-from=/home/uploading_list --checkers 1 --transfers 1 --use-mmap --buffer-size 0M --tpslimit 1 --no-traverse --config=/rclone.conf
 cat /home/up_ing_list >>/home/up_ed_list
 done
 #--checkers 1 --transfers 1 --use-mmap --buffer-size 0M --tpslimit 1 --no-traverse
