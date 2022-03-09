cbbf30a88a01d5d8ec27429d957d89f57aba4241
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-09 07:37:22 +0530

    Update rclone.sh

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #VPS
+rclone ls /jdx/downloads >/home/up_ed_list
 while :
 do
 sleep 30
-thelog="$(ls /jdx/logs |grep '2022' |tail -1)"
-cat /jdx/logs/"$thelog"/* |grep 'DownloadCandidate:' |sed 's/DownloadCandidate:/\nFILE="/g' |sed 's/@/"\n/g' |grep 'FILE="' |sort -u |sed 's|$|; rclone copy "/jdx/jdownload/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf --checkers 1 --transfers 1 --use-mmap --buffer-size 0M --tpslimit 1 --no-traverse; rclone copy "/jdx/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf --checkers 1 --transfers 1 --use-mmap --buffer-size 0M --tpslimit 1 --no-traverse|g' |sed '/FILE=" ; rclone/d' |bash
+rclone ls /jdx/downloads >/home/up_list
+grep -v -x -f /home/up_ed_list /home/up_list |sed 's/^ *//g' |
 done
 #--checkers 1 --transfers 1 --use-mmap --buffer-size 0M --tpslimit 1 --no-traverse
