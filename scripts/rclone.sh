6e2257c738db509dbd3bd6a15bfe7602eb4f2d19
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-09 10:45:10 +0530

    Update rclone.sh

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -5,6 +5,7 @@ while :
 do
 sleep 30
 rclone ls /jdx/downloads >/home/up_list
-grep -v -x -f /home/up_ed_list /home/up_list |sed 's/^ *//g' |
+grep -v -x -f /home/up_ed_list /home/up_list |sed 's/^ *//g' |sed '/\.part/d' >/home/up_ing_list
+rclone _-
 done
 #--checkers 1 --transfers 1 --use-mmap --buffer-size 0M --tpslimit 1 --no-traverse
