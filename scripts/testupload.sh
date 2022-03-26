62ae1ad621d0f46e6947d14c7db908656a6dc829
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-26 18:45:27 +0530

    Update testupload.sh

--- scripts/testupload.sh
+++ scripts/testupload.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+curl "$conf_in_url" >/rclone.conf
+firstremote=$(rclone listremotes --config=/rclone.conf |head -1 |sed 's/://g')
 mkdir /home/uplist
-ls -A >/home/uplist/cl1.txt
+rclone lsf -R /jdx >/home/uplist/initlist.txt
 while :
 do
-   ls -A >/home/uplist/cl2.txt
-   grep -v -x -f /home/uplist/cl1.txt /home/uplist/cl2.txt >/home/uplist/filter.txt
-   cat /home/uplist/cl2.txt /home/uplist/cl1.txt >>/home/uplist/blacklist.txt
-   cat /home/uplist/blacklist.txt >/home/uplist/cl2.txt
-#   rclone copy ......
+         cat /home/uplist/initlist.txt | sed "s/^/- /g" >/home/uplist/minus.txt
+         rclone lsf -R /jdx >/home/uplist/plist.txt #present list
+         grep -v -x -f /home/uplist/plist.txt /home/uplist/initlist.txt | sed "s/^/+ /g" >/home/uplist/plus.txt
+         cat /home/uplist/plus.txt /home/uplist/minus.txt >/home/uplist/filter.txt
+         rclone copy --files-from /home/uplist/filter.txt /jdx "$firstremote":jd2rc --config=/rclone.conf
+                 
+
+         cat /home/uplist/plist.txt >>/home/uplist/initlist.txt 
+         
+
+#    ls -A >/home/uplist/cl2.txt
+#    grep -v -x -f /home/uplist/cl1.txt /home/uplist/cl2.txt | sed "s/^/+ /g" >/home/uplist/filter.txt
+#    cat /home/uplist/cl2.txt | sed "s/^/- /g" > 
+#    rclone copy --include file.txt 
    
    
    
