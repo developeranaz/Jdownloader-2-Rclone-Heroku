31d8c03401e303cf78467cf38f8f949232f38f12
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-26 15:23:18 +0530

    Update testupload.sh

--- scripts/testupload.sh
+++ scripts/testupload.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 mkdir /home/uplist
-ls -A >/home/uplist/current_list.txt
+ls -A >/home/uplist/cl1.txt
 while :
 do
-   ls -A >/home/uplist/current_list.txt
+   ls -A >/home/uplist/cl2.txt
+   grep -v -x -f /home/uplist/cl1.txt /home/uplist/cl2.txt >/home/uplist/filter.txt
+   cat /home/uplist/cl2.txt /home/uplist/cl1.txt >/home/uplist/blacklist.txt
+   cat /home/uplist/blacklist.txt >/home/uplist/cl2.txt
+#   rclone copy ......
    
    
    
